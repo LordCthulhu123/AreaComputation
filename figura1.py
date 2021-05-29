eede9e12b40b281baa03c4efa5e6ce4484a10790
LordCthulhu <1.mateusqwe12345@gmail.com>
2021-05-29 02:32:24 -0300

    Imagens explanatórias do método 1

--- figura1.py
+++ figura1.py
@@ -2,18 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import Metodo1
 
-grid = Metodo1.Grid(x0=-2, x1=2, y0=-2, y1=2, num_div_x=10, num_div_y=10)
+a, b = 2, 2
+n = 20
+
+grid = Metodo1.Grid(x0=-a, x1=a, y0=-a, y1=a, num_div_x=n, num_div_y=n)
 
 fig, axs = plt.subplots(1)
 
 for point in grid:
     axs.plot(point.x, point.y, "o", c="blue", markersize=1)
+    if (point.x**2 + point.y**2) <= 1:
+        x = np.linspace(point.x - a/n, point.x + a/n, 10)
+        y_up, y_down = (point.y + b/n) * np.ones(10), (point.y - b/n) * np.ones(10)
+        axs.fill_between(x, y_down, y_up, color="plum")
+        # print(point)
 
-for i in range(0, 11):
-    axs.plot(np.linspace(-2, 2, 100), (-2 + i * (4/10))*np.ones(100), "k")
+for i in range(0, n + 1):
+    axs.plot(np.linspace(-a, a, 100), (-b + i * ((2*b)/n))*np.ones(100), "k")
 
-for j in range(0, 11):
-    axs.plot((-2 + j * (4/10))*np.ones(100), np.linspace(-2, 2, 100), "k")
+for j in range(0, n + 1):
+    axs.plot((-a + j * ((2*a)/n))*np.ones(100), np.linspace(-b, b, 100), "k")
 
 angles = np.linspace(0, 2*np.pi, 1000)
 axs.plot(np.cos(angles), np.sin(angles))
